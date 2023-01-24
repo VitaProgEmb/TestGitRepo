d627861fd9ba786f405815ce01fb188d4c847f0c
vitaProgi <vitasound83@gmail.com>
2023-01-24 23:49:36 +0500

    add tsak sys fix

--- system/sys.c
+++ system/sys.c
@@ -4,7 +4,21 @@
 #include "sys.h"
 
 
+static const char *TAG = "system";
+TaskHandle_t sys_tsk;
+static void sys_task(void *arg)
+{
+  ESP_LOGI(TAG,"start sys task");
+  while (1)
+  {
+    
+  }
+  vTaskDelete(NULL);
+
+}
+
 void sys_init()
 {
-  
+  xTaskCreatePinnedToCore(sys_task,"sys task",2048,
+                          NULL,1,&sys_tsk,1);
 }
